7d3eda56c733d2719d8d8fa4a3cf32ad4052bd6d
Adson Leite <adsonferreira585@gmail.com>
2022-10-18 00:18:00 -0300

    controle-bancario

---
+++ entidade/BaseDeDados.java
@@ -0,0 +1,58 @@
+package entidade;
+
+public class BaseDeDados {
+
+	private int conta;
+	private String nome;
+	private double valor;
+
+	
+	public BaseDeDados() {
+
+	}
+
+    public BaseDeDados(int conta, String nome) {
+	    this.conta = conta;
+	    this.nome = nome;
+}
+	public BaseDeDados(int conta, String nome, double depositonovo) {
+		this.conta = conta;
+		this.nome = nome;
+		deposito(depositonovo);
+	}
+	
+
+	public int getConta() {
+		return conta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void deposito(double valor) {
+		this.valor += valor;
+	}
+
+	public void saque(double valor) {
+		this.valor -= valor + 5.00;
+	}
+	
+	public String toString() {
+		return nome
+				+ " sua conta é "
+				+ conta
+				+ " seu saldo atual é de: "
+				+ String.format("%.2f", valor) + " R$";
+		
+	}
+
+}
